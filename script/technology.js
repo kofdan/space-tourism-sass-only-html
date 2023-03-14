f014cc5d7abf481bde66dd2c7739535f72c5766f
kofdan <kofdan@gmail.com>
2023-03-14 14:47:03 +0200

    fix_technology

--- script/technology.js
+++ script/technology.js
@@ -10,18 +10,23 @@ const getData = async function () {
     console.log(error.message);
   }
 };
-
+const desctopWidth = 1439;
 const initTechno = 0;
 const init = async () => {
   const { technology: technologys } = await getData();
   const techno = technologys.find((_, i) => i === initTechno);
+  tabs[0].classList.add("tab-active");
   renderTechnology(techno);
 };
 
 function renderTechnology(techno) {
   clearContent();
   const imgHtml = `<div class="technology__img">
-                        <img src=".${techno.images.landscape}" alt="${techno.name}" />
+                        <img src=".${
+                          document.documentElement.scrollWidth <= desctopWidth
+                            ? techno.images.landscape
+                            : techno.images.portrait
+                        }" alt="${techno.name}" />
                    </div>`;
   const descHtml = `<div class="technology__desc">
                         <h3>The terminology...</h3>
@@ -46,8 +51,11 @@ const clearContent = () => {
 
 async function tabHandler(e) {
   const { technology: technologys } = await getData();
+  const tab = e.target;
   const { id } = e.target.dataset;
   const techno = technologys.find((_, i) => i === +id);
+  tabs.forEach((tab) => tab.classList.remove("tab-active"));
+  tab.classList.add("tab-active");
   renderTechnology(techno);
 }
 
